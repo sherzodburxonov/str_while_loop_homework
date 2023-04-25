cc8d5d878a6914e699999c1910ecf055fdc62339
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-25 09:10:03 +0500

    03

--- While03.py
+++ While03.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    n=0
+    while n<len(s):
+        if s[n].isalpha():
+            i=i+1
+        n=n+1
+    return len(s)-i
+print(main("#hashtag@$"))
