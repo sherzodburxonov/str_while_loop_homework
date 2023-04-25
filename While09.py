6b455789bdb6a66bea49ee8df4bcf921b993a674
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-25 09:49:09 +0500

    09

--- While09.py
+++ While09.py
@@ -6,4 +6,10 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    n=0
+    while n<len(s):
+        i=int(s[n])+i
+        n=n+1
+    return i
+print(main("5468745645"))
