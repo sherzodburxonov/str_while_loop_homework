2021f86596c1f5ce364ba4550118a33005897d1a
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-25 12:51:40 +0500

    06

--- While06.py
+++ While06.py
@@ -7,3 +7,21 @@ def main(s):
     Returns:
         int: return answer
     """
+    i=0
+    n=0
+    while n<len(s):
+        if s[n]=="a" :
+            i=i+1
+        if s[n]=="e" :
+            i=i+1
+        if s[n]=="i" :
+            i=i+1
+        if s[n]=="o" :
+            i=i+1
+        if s[n]=="u" :
+            i=i+1
+        if s[n]=="o'" :
+            i=i+1
+        n=n+1
+    return len(s)-i
+print(main("Codeacademy"))
